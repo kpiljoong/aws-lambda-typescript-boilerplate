c2f59faf936524083e8577970781a08a7401124b
piljoong <kpiljoong@gmail.com>
2023-05-08 01:29:08 +0900

    first commit

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,16 +1,34 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { FunctionUrlAuthType } from "aws-cdk-lib/aws-lambda";
+import * as path from "path";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const fn = new NodejsFunction(this, "FunctionHandler", {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: "handler",
+      entry: path.join(__dirname, "../lambda/sample/index.ts"),
+      bundling: {
+        minify: true,
+        externalModules: ["aws-sdk"],
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const fnUrl = fn.addFunctionUrl({
+      authType: FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins: ["*"],
+      },
+    });
+
+    new cdk.CfnOutput(this, "FunctionUrl", {
+      value: fnUrl.url,
+    });
   }
 }
